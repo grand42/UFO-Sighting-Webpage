8323eac4a1b92cbe3560dbe6309e825eb3926e1a
Gretchen A <and01487@umn.edu>
2021-01-07 22:16:08 -0600

    Code filter by date

--- UFO-level 1/static/js/app.js
+++ UFO-level 1/static/js/app.js
@@ -6,13 +6,12 @@ var button = d3.select("#filter-btn");
 
 // Select form
 var form_control = d3.select("#datetime");
+var form = d3.select("#form")
 
 // Select table body
 var tbody = d3.select("tbody");
 
-// Create event handlers 
-//button.on("click", runEnter);
-//form.on("submit",runEnter);
+
 
 data.forEach((Sighting) => {
     var row = tbody.append("tr");
@@ -21,3 +20,34 @@ data.forEach((Sighting) => {
       cell.text(value);
     });
   });
+
+  // Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear table
+    tbody.html("")
+
+    // Get the value property of the input element
+    var inputValue = form_control.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+
+    filteredData.forEach((Sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(Sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+    }
