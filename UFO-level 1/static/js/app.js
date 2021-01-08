ad5c8260ecf7bbe05b41216f06e897381b24af34
Gretchen A <and01487@umn.edu>
2021-01-07 21:10:50 -0600

    Add references

--- UFO-level 1/static/js/app.js
+++ UFO-level 1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select button
+var button = d3.select("#filter-btn");
+
+// Select form
+var form_control = d3.select(#datetime);
+
+// Select table body
+var tbody = d3.select("tbody");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
