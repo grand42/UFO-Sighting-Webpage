2b7d3aa33478e8a48f3fe7b1bfca040664f54891
Gretchen A <and01487@umn.edu>
2021-01-07 21:31:08 -0600

    Add table to html

--- UFO-level 1/static/js/app.js
+++ UFO-level 1/static/js/app.js
@@ -5,12 +5,19 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 
 // Select form
-var form_control = d3.select(#datetime);
+var form_control = d3.select("#datetime");
 
 // Select table body
 var tbody = d3.select("tbody");
 
 // Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+//button.on("click", runEnter);
+//form.on("submit",runEnter);
 
+data.forEach((Sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(Sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
